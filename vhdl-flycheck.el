be89151515d888fdbdfd9568d819bbd37d6844b9
Gonzalo Larumbe <gonzalomlarumbe@gmail.com>
2023-01-28 15:56:16 +0100

    Add some utils, refactor flycheck

--- vhdl-flycheck.el
+++ vhdl-flycheck.el
@@ -30,25 +30,7 @@
 (require 'vhdl-utils)
 
 
-(defun vhdl-ext-flycheck-dirs-of-open-buffers ()
-  "Return a list of directories from current VHDL opened files.
-Used for `ghdl' linter flycheck include directories."
-  (let ((vhdl-opened-dirs nil))
-    (dolist ($buf (buffer-list (current-buffer)))
-      (with-current-buffer $buf
-        (when (string-equal major-mode "vhdl-mode")
-          (setq vhdl-opened-dirs (push default-directory vhdl-opened-dirs)))))
-    vhdl-opened-dirs))
-
-(defun vhdl-ext-flycheck-get-standard ()
-  "Get current standard as a string from `vhdl-standard'."
-  (let ((std (car vhdl-standard)))
-    (if (equal std 8)
-        (format "0%s" std)
-      (format "%s" std))))
-
-
-(flycheck-def-option-var flycheck-ghdl-include-path nil vhdl-ghdl
+(flycheck-def-option-var vhdl-ext-flycheck-ghdl-include-path nil vhdl-ghdl
   "A list of include directories for GHDL.
 
 List of strings where each is a directory to be added to the include path of
@@ -57,7 +39,7 @@ GHDL."
   :safe #'flycheck-string-list-p
   :package-version '(flycheck . "32"))
 
-(flycheck-def-option-var flycheck-ghdl-work-lib vhdl-default-library vhdl-ghdl
+(flycheck-def-option-var vhdl-ext-flycheck-ghdl-work-lib vhdl-default-library vhdl-ghdl
   "Work library name to be used for GHDL."
   :type '(choice (const :tag "Default library" vhdl-default-library)
                  (string :tag "Custom work library"))
@@ -73,19 +55,12 @@ See URL `https://github.com/ghdl/ghdl'."
             (option "--workdir=" flycheck-ghdl-workdir concat)
             (option "--ieee=" flycheck-ghdl-ieee-library concat)
             ;; Additional options
-            (option-list "-P" flycheck-ghdl-include-path concat)
-            (option "--work=" flycheck-ghdl-work-lib concat)
+            (option-list "-P" vhdl-ext-flycheck-ghdl-include-path concat)
+            (option "--work=" vhdl-ext-flycheck-ghdl-work-lib concat)
             source)
   :error-patterns
   ((error line-start (file-name) ":" line ":" column ": " (message) line-end))
   :modes (vhdl-mode vhdl-ts-mode))
-
-
-(defun vhdl-ext-flycheck-ghdl-hook ()
-  "Set GHDL flycheck options based on context."
-  (setq flycheck-ghdl-language-standard (vhdl-ext-flycheck-get-standard))
-  (setq flycheck-ghdl-include-path (vhdl-ext-flycheck-dirs-of-open-buffers))
-  (setq flycheck-ghdl-work-lib (vhdl-work-library)))
 
 
 
--- vhdl-utils.el
+++ vhdl-utils.el
@@ -46,6 +46,128 @@
 (defconst vhdl-ext-architecture-re "^\\s-*\\(architecture\\)\\s-+\\(\\(\\w\\|\\s_\\)+\\s-+of\\s-+\\(\\w\\|\\s_\\)+\\)")
 (defconst vhdl-ext-context-re "^\\s-*\\(context\\)\\s-+\\(\\(\\w\\|\\s_\\)+\\)")
 
+(defvar vhdl-ext-buffer-list nil)
+(defvar vhdl-ext-dir-list nil)
+
+
+(defun vhdl-ext-path-join (arg1 arg2)
+  "Join path of ARG1 and ARG2."
+  (if (and arg1 arg2)
+      (concat (file-name-as-directory arg1) arg2)
+    (error "Cannot join path with nil arguments")
+    nil))
+
+(defun vhdl-ext-replace-regexp (regexp to-string start end)
+  "Wrapper function for programatic use of `replace-regexp'.
+Replace REGEXP with TO-STRING from START to END."
+  (let* ((marker (make-marker))
+         (endpos (when end (set-marker marker end))))
+    (save-excursion
+      (goto-char start)
+      (while (re-search-forward regexp endpos t)
+        (replace-match to-string)))))
+
+(defun vhdl-ext-replace-regexp-whole-buffer (regexp to-string)
+  "Replace REGEXP with TO-STRING on whole `current-buffer'."
+  (vhdl-ext-replace-regexp regexp to-string (point-min) nil))
+
+(defun vhdl-ext-replace-string (string to-string start end &optional fixedcase)
+  "Wrapper function for programatic use of `replace-string'.
+Replace STRING with TO-STRING from START to END.
+
+If optional arg FIXEDCASE is non-nil, do not alter the case of
+the replacement text (see `replace-match' for more info)."
+  (let* ((marker (make-marker))
+         (endpos (when end (set-marker marker end))))
+    (save-excursion
+      (goto-char start)
+      (while (search-forward string endpos t)
+        (replace-match to-string fixedcase)))))
+
+(defun vhdl-ext-scan-buffer-entities ()
+  "Find entities in current buffer.
+Return list with found entities or nil if not found."
+  (let (entities)
+    (save-excursion
+      (goto-char (point-min))
+      (while (vhdl-re-search-forward vhdl-ext-entity-re nil t)
+        (push (match-string-no-properties 2) entities)))
+    (delete-dups entities)))
+
+(defun vhdl-ext-read-file-entities (&optional file)
+  "Find entities in current buffer.
+Find entities in FILE if optional arg is non-nil.
+Return list with found entities or nil if not found."
+  (let ((buf (if file
+                 (get-file-buffer file)
+               (current-buffer)))
+        (debug nil))
+    (if buf
+        (with-current-buffer buf
+          (vhdl-ext-scan-buffer-entities))
+      ;; If FILE buffer is not being visited, use a temporary buffer
+      (with-temp-buffer
+        (when debug
+          (clone-indirect-buffer-other-window "*debug*" t))
+        (insert-file-contents file)
+        (vhdl-ext-scan-buffer-entities)))))
+
+(defun vhdl-ext-select-file-entity (&optional file)
+  "Select file entity from FILE.
+If only one entity was found return it as a string.
+If more than one entity was found, select between available ones.
+Return nil if no entity was found."
+  (let ((entities (vhdl-ext-read-file-entities file)))
+    (if (cdr entities)
+        (completing-read "Select entity: " entities)
+      (car entities))))
+
+(defun vhdl-ext-project-root ()
+  "Find current project root, depending on available packages."
+  (or (when (featurep 'projectile)
+        (projectile-project-root))
+      (when (featurep 'project)
+        (project-root (project-current)))
+      (when (featurep 'ggtags)
+        (ggtags-current-project-root))
+      default-directory))
+
+(defun vhdl-ext-update-buffer-and-dir-list ()
+  "Update Vhdl-mode opened buffers and directories lists."
+  (let (vhdl-buffers vhdl-dirs)
+    (dolist (buf (buffer-list (current-buffer)))
+      (with-current-buffer buf
+        (when (or (string-equal major-mode "vhdl-mode")
+                  (string-equal major-mode "vhdl-ts-mode"))
+          (push buf vhdl-buffers)
+          (unless (member default-directory vhdl-dirs)
+            (push default-directory vhdl-dirs)))))
+    (setq vhdl-ext-buffer-list vhdl-buffers)
+    (setq vhdl-ext-dir-list vhdl-dirs)))
+
+(defun vhdl-ext-get-standard ()
+  "Get current standard as a string from `vhdl-standard'."
+  (let ((std (car vhdl-standard)))
+    (if (equal std 8)
+        (format "0%s" std)
+      (format "%s" std))))
+
+
+;;;; Hooks
+(defun vhdl-ext-open-buffer-hook ()
+  "VHDL hook to run when opening a file."
+  (vhdl-ext-update-buffer-and-dir-list)
+  (setq flycheck-ghdl-language-standard (vhdl-ext-get-standard))
+  (setq vhdl-ext-flycheck-ghdl-include-path vhdl-ext-dir-list)
+  (setq vhdl-ext-flycheck-ghdl-work-lib (vhdl-work-library)))
+
+(defun vhdl-ext-kill-buffer-hook ()
+  "VHDL hook to run when killing a buffer."
+  (setq vhdl-ext-buffer-list (remove (current-buffer) vhdl-ext-buffer-list)))
+
+(add-hook 'vhdl-mode-hook #'vhdl-ext-open-buffer-hook)
+(add-hook 'kill-buffer-hook #'vhdl-ext-kill-buffer-hook nil :local)
+
 
 
 (provide 'vhdl-utils)
