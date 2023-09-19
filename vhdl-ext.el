53df7c40f954fd40e542db51333a3abfa6df5ad3
Gonzalo Larumbe <gonzalomlarumbe@gmail.com>
2023-09-19 18:15:20 +0200

    Bump to version 0.3.1

--- vhdl-ext.el
+++ vhdl-ext.el
@@ -4,7 +4,7 @@
 
 ;; Author: Gonzalo Larumbe <gonzalomlarumbe@gmail.com>
 ;; URL: https://github.com/gmlarumbe/vhdl-ext
-;; Version: 0.3.0
+;; Version: 0.3.1
 ;; Keywords: VHDL, IDE, Tools
 ;; Package-Requires: ((emacs "29.1") (vhdl-ts-mode "0.1.0") (eglot "1.9") (lsp-mode "8.0.0") (ag "0.48") (ripgrep "0.4.0") (hydra "0.15.0") (flycheck "32") (outshine "3.0.1") (async "1.9.7"))
 
