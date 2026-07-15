7aecaa634140af3b69ac29e7e28cd9309dd872a7
Gonzalo Larumbe <gonzalomlarumbe@gmail.com>
2026-07-15 19:09:47 +0200

    Update copyright year

--- test/src/vhdl-ext-test-beautify.el
+++ test/src/vhdl-ext-test-beautify.el
@@ -1,6 +1,6 @@
 ;;; vhdl-ext-test-beautify.el --- vhdl-ext ERT beautify tests  -*- lexical-binding: t -*-
 
-;; Copyright (C) 2022-2025 Gonzalo Larumbe
+;; Copyright (C) 2022-2026 Gonzalo Larumbe
 
 ;; Author: Gonzalo Larumbe <gonzalomlarumbe@gmail.com>
 ;; URL: https://github.com/gmlarumbe/test-hdl
